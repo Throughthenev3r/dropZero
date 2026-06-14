92cc193ff78a8d9017aa318d2de2c92501d7cd9e
ThroughTheNev3r <skoore.steeno@gmail.com>
2026-06-13 20:58:56 -0400

    Remove duplicate and example tests

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes/index.js";
-import { generateCode } from "./utils/generateCode.js";
-console.log(generateCode());
+import db from "./db/database.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
+
+console.log("DB ok", db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all());
+
 
 app.listen(PORT, () => {
   console.log(`Server running at ${process.env.BASE_URL || `http://localhost:${PORT}`}`);
